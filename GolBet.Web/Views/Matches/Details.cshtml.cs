421c14b29be53c3f868bc4085f3bcc94367d824e
LUKASTR296 <lukastorocardona@gmail.com>
2026-09-16 18:58:33 -0500

    Add match detail page and board filters (Module 5)

---
+++ GolBet.Web/Views/Matches/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GolBet.Web.Views.Matches
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
